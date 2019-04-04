eca3daf73332cc923153846790555c3754551d92
willidaconceicao <willidaconceicao@gmail.com>
2019-04-05 01:24:08 +0200

    Updates

---
+++ precache-manifest.2042df7942850eceecea2770e926e184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4ad8b7237d6d3629d1f",
+    "url": "/portefolioV2/static/css/main.6f14bdb7.chunk.css"
+  },
+  {
+    "revision": "b4ad8b7237d6d3629d1f",
+    "url": "/portefolioV2/static/js/main.b4ad8b72.chunk.js"
+  },
+  {
+    "revision": "19bd9cef750c88b59e77",
+    "url": "/portefolioV2/static/js/runtime~main.19bd9cef.js"
+  },
+  {
+    "revision": "3902d8d9956139092d90",
+    "url": "/portefolioV2/static/js/2.3902d8d9.chunk.js"
+  },
+  {
+    "revision": "a35f1044e1fc73977b46e1b563e4821b",
+    "url": "/portefolioV2/static/media/about.a35f1044.jpg"
+  },
+  {
+    "revision": "223e8286b16330876bfdce97b87916cc",
+    "url": "/portefolioV2/index.html"
+  }
+];
