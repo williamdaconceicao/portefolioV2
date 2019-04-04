d122917730643ed7e950f293ef02711f561cb988
williamdaconceicao <willidaconceicao@gmail.com>
2019-04-05 01:12:26 +0200

    MyFirstCommit

---
+++ precache-manifest.1a0a022048b3320468a7269f5b744d49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9458e88aa68bd7ce1c1a",
+    "url": "/static/css/main.6f14bdb7.chunk.css"
+  },
+  {
+    "revision": "9458e88aa68bd7ce1c1a",
+    "url": "/static/js/main.9458e88a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3902d8d9956139092d90",
+    "url": "/static/js/2.3902d8d9.chunk.js"
+  },
+  {
+    "revision": "a35f1044e1fc73977b46e1b563e4821b",
+    "url": "/static/media/about.a35f1044.jpg"
+  },
+  {
+    "revision": "327b28242f8c3feb737daf5124829e9f",
+    "url": "/index.html"
+  }
+];
